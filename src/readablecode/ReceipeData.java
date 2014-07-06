6f392b70902c2c8ed3b8dfd31c20fa1122e17d80
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2014-07-06 11:48:09 +0900

    spec3: add ReceipeData.java

---
+++ src/readablecode/ReceipeData.java
@@ -0,0 +1,13 @@
+package readablecode;
+
+public class ReceipeData {
+	private String mRecipeName;
+	
+	public ReceipeData(String name) {
+		mRecipeName = name;
+	}
+	
+	public String getRecipeName() {
+		return mRecipeName;
+	}
+}
