b5f36ec42ad7fc4d7d97a387f61a7600e359a25c
gecko655 <aqwsedrft1234@yahoo.co.jp>
2014-07-06 14:38:09 +0900

    spec6: Implemented spec6 "output only the recipe of specified id

--- src/readablecode/Main.java
+++ src/readablecode/Main.java
@@ -22,10 +22,42 @@ public class Main {
 		
 		String recipeDataFilePath = args[0];
 		File recipeFile = new File(recipeDataFilePath);
+		if(args.length==1){
+			printRecipe(recipeFile);
+		}else if(args.length==2){
+			if(isNaturalNumber(args[1])){
+				long specifiedId = Long.valueOf(args[1]);
+				printRecipe(recipeFile,specifiedId);
+			}else{
+				System.err.println("第2引数は整数値を与えてください");
+				System.exit(1);
+			}
+		}
+	}
+	
+	private static void printRecipe(File recipeFile) {
+		printRecipe(recipeFile,0);
+	}
+
+	/**
+	 * Print the recipe of given specifiedId.
+	 * If specifiedId is 0, it prints all of recipes.
+	 * @param recipeFile
+	 * @param specifiedId
+	 */
+	private static void printRecipe(File recipeFile, long specifiedId) {
+		boolean isNotSpecifiedId = (specifiedId == 0);
+		boolean found = false;
 		try {
-			ArrayList<RecipeData> receipeDataList = readRecipeData(recipeFile);
-			for(RecipeData data : receipeDataList) {
-				System.out.println(data.getRecipeId()+ ": "+ data.getRecipeName());
+			ArrayList<RecipeData> recipeDataList = readRecipeData(recipeFile);
+			for(RecipeData data: recipeDataList){
+				if(data.getRecipeId()==specifiedId||isNotSpecifiedId){
+					System.out.println(data.getRecipeId()+ ": "+ data.getRecipeName());
+					found =true;
+				}
+			}
+			if(!found){
+				System.err.println("指定されたIDのレシピが存在しません。");
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -34,8 +66,23 @@ public class Main {
 			e.printStackTrace();
 			System.err.println("レシピデータの読み込みに失敗しました．");
 		}
+		
 	}
-	
+
+	private static boolean isNaturalNumber(String string) {
+		long l=-1;
+		try{
+			l=Long.valueOf(string);
+		}catch(Exception e){
+			//NumberFormatException and NullPointerException are considered.
+			return false;
+		}
+		if(l<=0){
+			return false;
+		}
+		return true;
+	}
+
 	public static ArrayList<RecipeData> readRecipeData(File receipeDataFile) throws FileNotFoundException, IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(receipeDataFile));
 		ArrayList<RecipeData> receipeDataList = new ArrayList<RecipeData>();
